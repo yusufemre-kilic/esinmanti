bbe95f41ca7321ef561ebf7aeac8562c5ff825cf
yusufemre-kilic <yusufemre.kilic@outlook.com>
2025-09-30 15:14:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -65,6 +65,11 @@ next.addEventListener('click', nextCard);
 prev.addEventListener('click', prevCard);
 
 
+let activeChefItems = [...document.getElementsByClassName('slider-item')].length;
+let chefItemWidth = sliderItem.offsetWidth;
+const nextItem = document.querySelector('.chef-main-slider .next');
+const prevItem = document.querySelector('.chef-main-slider .prev');
+const movedItemChef = document.querySelector('.moved-chef-slider');
 
 let leftMove = 0;
 let allowedShowedChef = 1;
