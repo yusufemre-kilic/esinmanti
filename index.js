f0629614294527fbc8cbc667a6404d27951f311a
yusufemre-kilic <yusufemre.kilic@outlook.com>
2025-09-30 15:18:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -64,12 +64,13 @@ let prevCard = (e) => {
 next.addEventListener('click', nextCard);
 prev.addEventListener('click', prevCard);
 
-
-let activeChefItems = [...document.getElementsByClassName('slider-item')].length;
-let chefItemWidth = sliderItem.offsetWidth;
+let activeChefItems = [...document.getElementsByClassName('slider-item')]
+	.length;
+let chefItemWidth = document.querySelector('.slider-item').offsetWidth;
 const nextItem = document.querySelector('.chef-main-slider .next');
 const prevItem = document.querySelector('.chef-main-slider .prev');
 const movedItemChef = document.querySelector('.moved-chef-slider');
+
 
 let leftMove = 0;
 let allowedShowedChef = 1;
